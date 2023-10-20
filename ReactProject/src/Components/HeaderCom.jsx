1ebd0271d634d2586b306d68a7bf626882a58512
Dharmin Patel <dharminpate222l@gmail.com>
2023-10-20 11:48:44 +0530

    .

--- ReactProject/src/Components/HeaderCom.jsx
+++ ReactProject/src/Components/HeaderCom.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import {BsSearch} from 'react-icons/bs'
 
 export default function HeaderCom() {
   return (
@@ -25,27 +26,82 @@ export default function HeaderCom() {
       </header>
       <header className="header-upper py-3">
         <div className="container">
-          <div className="row">
+          <div className="row align-items-center">
             <div className="col-2">
               <h3>
-                <Link className="text-white">Dev Corner.</Link>
+                <Link className="text-white">Dev Corner</Link>
               </h3>
             </div>
             <div className="col-5">
-              <div className="input-group mb-3">
+              <div className="input-group ">
                 <input
                   type="text"
-                  className="form-control"
-                  placeholder="Recipient's username"
-                  aria-label="Recipient's username"
+                  className="form-control py-2"
+                  placeholder="Search Product Here"
+                  aria-label="Search Product Here"
                   aria-describedby="basic-addon2"
                 />
-                <span className="input-group-text" id="basic-addon2">
-                  @example.com
+                <span className="input-group-text p-3" id="basic-addon2">
+                  <BsSearch className="fs-6" />
                 </span>
               </div>
             </div>
-            <div className="col-5"></div>
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/compare.svg" alt="compare" />
+                    <p className="mb-0">
+                      Compare <br /> Products
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/wishlist.svg" alt="wishlist" />
+                    <p className="mb-0">
+                      Favourite <br /> wishlist
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/user.svg" alt="user" />
+                    <p className="mb-0">
+                      Log in <br /> My Account
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/cart.svg" alt="cart" />
+                    <div className="d-flex flex-column gap-10">
+                      <span className="badge bg-white text-dark">0</span>
+                      <p className="mb-0">$500</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-bottom py-3">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="menu-button d-flex align-items-center">
+                <div></div>
+                <div className="menu-links">
+                  <div className="d-flex align-items-center gap-15">
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/">Our Store</NavLink>
+                    <NavLink to="/">Blog</NavLink>
+                    <NavLink to='/contact'>Contact</NavLink>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </header>
