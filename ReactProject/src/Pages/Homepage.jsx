dab16a69620438523eb138d1b921466b867e7785
Dharmin Patel <dharminpate222l@gmail.com>
2023-10-21 15:28:11 +0530

    .

--- ReactProject/src/Pages/Homepage.jsx
+++ ReactProject/src/Pages/Homepage.jsx
@@ -8,7 +8,7 @@ export default function Homepage() {
         <div className="container">
           <div className="row">
             <div className="col-6">
-              <div className="main-banner p-3 position-relative">
+              <div className="main-banner position-relative">
                 <img
                   src="images/main-banner-1.jpg"
                   className="img-fluid rounded-3"
@@ -23,20 +23,174 @@ export default function Homepage() {
               </div>
             </div>
             <div className="col-6">
-              <div className="d-flex flex-wrap justity-content-between align-item-center">
-                  <div className="small-banner p-3 position-relative">
-                <img
-                  src="images/catbanner-01.jpg"
-                  className="img-fluid rounded-3"
-                  alt=""
-                />
-                <div className="small-banner-content position-absolute">
-                  <h4>SUPERCHARGED FOR PROS.</h4>
-                  <h5>IPAD S13+ Pro.</h5>
-                  <p>From $999.00 or $41.62/mo.</p>
-                  <Link className="button"> BUY NOW</Link>
+              <div className="d-flex flex-wrap gap-10 justity-content-between align-item-center">
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-01.jpg"
+                    className="img-fluid rounded-3"
+                    alt=""
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>SUPERCHARGED FOR PROS.</h4>
+                    <h5>IPAD S13+ Pro.</h5>
+                    <p>
+                      From $999.00 <br /> or $41.62/mo.
+                    </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-02.jpg"
+                    className="img-fluid rounded-3"
+                    alt=""
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>NEW ARRIVAL</h4>
+                    <h5>Buy Ipad Air</h5>
+                    <p>
+                      From $599.00 <br /> or $49.91/mo. for 12 mo.
+                    </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-03.jpg"
+                    className="img-fluid rounded-3"
+                    alt=""
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>NEW ARRIVAL</h4>
+                    <h5>But IPad Air</h5>
+                    <p>
+                      From $999.00 <br /> or $41.62/mo.
+                    </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-04.jpg"
+                    className="img-fluid rounded-3"
+                    alt=""
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>SUPERCHARGED FOR PROS.</h4>
+                    <h5>IPAD S13+ Pro.</h5>
+                    <p>
+                      From $999.00 <br /> or $41.62/mo.
+                    </p>
+                  </div>
                 </div>
               </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="home-wrapper-2 py-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="service d-flex align-item-center justify-content-between">
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service.png" alt="" />
+                  <div>
+                    <h6>Free Shipping</h6>
+                    <p className="mb-0">From all order over $100</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service-02.png" alt="" />
+                  <div>
+                    <h6>Daily Surprise Offers</h6>
+                    <p className="mb-0">Save up to 25% off</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service-03.png" alt="" />
+                  <div>
+                    <h6>Support 24/7</h6>
+                    <p className="mb-0">Shop with an export</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service-04.png" alt="" />
+                  <div>
+                    <h6>Affordable Prices</h6>
+                    <p className="mb-0">Get Factory direct price</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service-05.png" alt="" />
+                  <div>
+                    <h6>Secure Payments</h6>
+                    <p className="mb-0">100% Protected Payments</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="home-wrapper-3 py-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="categories d-flex flex-wrap align-items-center justify-content-between">
+                <div className="d-flex align-items-center gap-30">
+                  <div>
+                    <h6>Cameras</h6>
+                    <p>10 Items</p>
+                  </div>
+                  <img src="images/camera.jpg" alt="" />
+                </div>
+                <div className="d-flex align-items-center gap-30">
+                  <div>
+                    <h6>Smart Tv</h6>
+                    <p>10 Items</p>
+                  </div>
+                  <img src="images/tv.jpg" alt="" />
+                </div>
+                <div className="d-flex align-items-center gap-30">
+                  <div>
+                    <h6>Music & Gaming</h6>
+                    <p>10 Items</p>
+                  </div>
+                  <img src="images/headphone.jpg" alt="" />
+                </div>
+                <div className="d-flex align-items-center gap-30">
+                  <div>
+                    <h6>Laptops</h6>
+                    <p>10 Items</p>
+                  </div>
+                  <img src="images/laptop.jpg" alt="" />
+                </div>
+                <div className="d-flex align-items-center gap-30">
+                  <div>
+                    <h6>Laptops</h6>
+                    <p>10 Items</p>
+                  </div>
+                  <img src="images/laptop.jpg" alt="" />
+                </div>
+                <div className="d-flex align-items-center gap-30">
+                  <div>
+                    <h6>Smart Tv</h6>
+                    <p>10 Items</p>
+                  </div>
+                  <img src="images/tv.jpg" alt="" />
+                </div>
+                <div className="d-flex align-items-center gap-30">
+                  <div>
+                    <h6>Music & Gaming</h6>
+                    <p>10 Items</p>
+                  </div>
+                  <img src="images/headphone.jpg" alt="" />
+                </div>
+                <div className="d-flex align-items-center gap-30">
+                  <div>
+                    <h6>Cameras</h6>
+                    <p>10 Items</p>
+                  </div>
+                  <img src="images/camera.jpg" alt="" />
+                </div>
               </div>
             </div>
           </div>
